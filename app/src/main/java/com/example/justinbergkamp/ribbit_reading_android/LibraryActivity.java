24ed8b4186f4dbb5879045b553b4151a0e93304d
jtb43 <j.bergkamp2@gmail.com>
2018-04-01 23:00:34 -0400

    Added DOM parser to Library page

--- app/src/main/java/com/example/justinbergkamp/ribbit_reading_android/LibraryActivity.java
+++ app/src/main/java/com/example/justinbergkamp/ribbit_reading_android/LibraryActivity.java
@@ -1,11 +1,18 @@
 package com.example.justinbergkamp.ribbit_reading_android;
-
+import com.example.justinbergkamp.ribbit_reading_android.R;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import java.io.InputStream;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -15,16 +22,45 @@ public class LibraryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_library);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        LinearLayout layout = findViewById(R.id.activity_library);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        try {
+
+            InputStream is = getAssets().open("friends.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+
+            Document doc = dBuilder.parse(is);
+
+            Element element = doc.getDocumentElement();
+
+            element.normalize();
+            NodeList nodeList = doc.getElementsByTagName("employee");
+            for (int i = 0; i < nodeList.getLength(); i++) {
+
+                TextView text1 = new TextView(this);
+                Node node = nodeList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element2 = (Element) node;
+                    text1.setTextSize(15);
+                    text1.setTextColor(0xFFFF0000);
+                    text1.setText(text1.getText() + "Name: " + getValue("name", element2) + "\n");
+                    text1.setText(text1.getText() + "Profession: " + getValue("profession", element2) + "\n");
+                    layout.addView(text1);
+                }
             }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    public static String getValue(String tag, Element e) {
+
+        NodeList node = e.getElementsByTagName(tag).item(0).getChildNodes();
+        Node nodes = node.item(0);
+        return nodes.getNodeValue();
+
+    }
+
 
 }
