6f129a6646ecf96491e13c53a401a7800212ddf7
jtb43 <j.bergkamp2@gmail.com>
2018-04-02 02:23:59 -0400

    Selecting cover images via xml files in assets

--- app/src/main/java/com/example/justinbergkamp/ribbit_reading_android/LibraryActivity.java
+++ app/src/main/java/com/example/justinbergkamp/ribbit_reading_android/LibraryActivity.java
@@ -1,9 +1,12 @@
 package com.example.justinbergkamp.ribbit_reading_android;
 import com.example.justinbergkamp.ribbit_reading_android.R;
+
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import org.w3c.dom.Document;
@@ -25,30 +28,30 @@ public class LibraryActivity extends AppCompatActivity {
         LinearLayout layout = findViewById(R.id.activity_library);
 
         try {
-
-            InputStream is = getAssets().open("friends.xml");
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-
-            Document doc = dBuilder.parse(is);
-
-            Element element = doc.getDocumentElement();
-
-            element.normalize();
-            NodeList nodeList = doc.getElementsByTagName("employee");
-            for (int i = 0; i < nodeList.getLength(); i++) {
-
-                TextView text1 = new TextView(this);
-                Node node = nodeList.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element2 = (Element) node;
-                    text1.setTextSize(15);
-                    text1.setTextColor(0xFFFF0000);
-                    text1.setText(text1.getText() + "Name: " + getValue("name", element2) + "\n");
-                    text1.setText(text1.getText() + "Profession: " + getValue("profession", element2) + "\n");
-                    layout.addView(text1);
+            String [] list = getAssets().list("stories");
+            System.out.println(list.length);
+            if (list.length > 0) {
+                for (String file : list) {
+                    String pls = "stories/"+file;
+                    InputStream is = getAssets().open(pls);
+                    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                    Document doc = dBuilder.parse(is);
+                    Element element = doc.getDocumentElement();
+                    element.normalize();
+                    Node cover_image = doc.getElementById("cover_image");
+                    //add to the screen as an ImageButton
+                    String ex = "pictures/"+cover_image.getTextContent();
+                    InputStream image_lookup = getAssets().open(ex);
+                    Drawable d = Drawable.createFromStream(image_lookup, null);
+                    ImageButton ib = new ImageButton(this);
+                    ib.setImageDrawable(d);
+                    layout.addView(ib);
+                    is.close();
+                    image_lookup.close();
                 }
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
