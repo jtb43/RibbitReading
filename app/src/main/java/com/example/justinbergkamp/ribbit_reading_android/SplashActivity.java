c331e7c39dd19aec7b3401d443cb07d23386e52d
Cody Li <codyli332@gmail.com>
2018-04-19 01:28:42 -0400

    added super janky method of leaving splash screen on for a bit longer

--- app/src/main/java/com/example/justinbergkamp/ribbit_reading_android/SplashActivity.java
+++ app/src/main/java/com/example/justinbergkamp/ribbit_reading_android/SplashActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
-
+import java.util.concurrent.TimeUnit;
 /**
  * Created by codyli on 4/19/18.
  */
@@ -16,6 +16,14 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+
+        //real janky method of making the splash screen stay on for a bit longer
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        }
+        catch(InterruptedException e){
+            System.out.print("Error");
+        }
         finish();
 
     }
